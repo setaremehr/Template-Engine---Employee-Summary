50cf849193d202ee6b8171f1c50640d95360bb4a
setare mehr <setaremehr89@gmail.com>
2020-09-24 14:01:23 -0700

    fixed app.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,14 +1,184 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const htmlRenderer = require('./lib/htmlRenderer');
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+let employees = [];
+
+async function addManager() {
+    const data = await inquirer.prompt([
+        {
+         type: 'input',
+            message: "what is the manager's name?",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: 'what is the manager email address?',
+            name: 'email'
+        },
+        {
+            type: 'input',
+            message: "what is the manager's id?",
+            name: 'id'
+        },
+        {
+            type: 'number',
+            message: "what is the manager's office number?",
+            name: 'officeNumber'
+        },
+    ]);
+
+
+   
+            const name = data.name
+            const email = data.email
+            const id = data.id
+            const officeNumber = data.officeNumber
+            const newManager = new Manager(name, id, email, officeNumber)
+            employees.push(newManager)
+            addMembers();
+       
+}
+
+async function addEngineer() {
+    const data = await inquirer.prompt([
+        {
+            type: 'input',
+            message: "what is the engineer's name?",
+            name: 'name'
+        },
+        {
+            type: 'input',
+            message: "what is the engineer's id?",
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's email address?",
+            name: 'email'
+        },
+        {
+            type: "input",
+            message: "What is this engineer's Github profile?",
+            name: "github"
+        }
+    ])
+        // .then(data => {
+            console.log(data);
+            const name = data.name
+            const id = data.id
+            const email = data.email
+            const github = data.github
+            const newEngineer = new Engineer(name, id, email, github)
+            employees.push(newEngineer)
+            addMembers();
+        // });
+}
+
+function addIntern() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the intern's name?",
+            name: "name"
+        },
+        {
+            type: 'input',
+            message: "what is the intern's id?",
+            name: 'id'
+        },
+        {
+            type: "input",
+            message: "What is the intern's email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is the intern's school?",
+            name: "school"
+        }
+    ])
+        .then(data => {
+            const name = data.name
+            const id = data.id
+            const email = data.email
+            const school = data.school
+            const newIntern = new Intern(name, id, email, school)
+            employees.push(newIntern)
+            addMembers();
+
+        });
+}
+
+async function addMembers() {
+    const data = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add more team members?',
+            choices: [
+                "Yes, add a manager",
+                "Yes, add an engineer",
+                "Yes, add an intern",
+                "No, it is already completed"
+            ],
+            name: "addMoreMember"
+        }
+    ])
+        // .then(data => {
+            switch (data.addMoreMember) {
+                case 'Yes, add an engineer':
+                    addEngineer();
+                    break;
+
+                    case 'Yes, add a manager':
+                        addManager();
+                        break;
+
+                case ('Yes, add an intern'):
+                    addIntern();
+                    break;
+
+                case ('No, it is already completed'):
+                    console.log("i am here");
+                    writehtml();
+                    break;
+            }
+        // });
+       
+}
+// addManager();
+addMembers();
+
+     function writehtml (){
+        console.log("say somthing");
+        console.log(employees);
+//         fs.writeFile('./finalHtml/final.html', 'utf8', function(err){
+//     if (err) {
+//         throw new Error(err);
+//       }
+// });
+const $html = htmlRenderer(employees);
+console.log($html);
+fs.writeFileSync(path.join("finalhtml","final.html"),$html);
+     }
+
+
+
+// const htmlRen = htmlRenderer(employees)
+// fs.writeFile('./finalHtml/final.html', 'utf8', function(err){
+//     if (err) {
+//         throw new Error(err);
+//       }
+// });
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -32,4 +202,4 @@ const render = require("./lib/htmlRenderer");
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// for the provided `render` function to work! `
